ccc1e3f6588d57531cd6e82b197651f4615a5215
FrayedBlade <kestrel_o7@proton.me>
2024-07-29 23:29:16 +0000

    Update hybrid_mode.sh

--- hybrid_mode.sh
+++ hybrid_mode.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ -f /etc/modprobe.d/vfio.conf ]; then
-    notify-send "Laptop je vec u hybrid modu!"
+    notify-send "Laptop already in hybrid mode!"
 else
     sudo mv /etc/modprobe.d/vfio.conff /etc/modprobe.d/vfio.conf
-    #postavlja laptop u hybrid mode
+    
+    #turns on hybrid mode in LLL
     sudo /home/kestrel/Programs/Toolkit/LenovoLegionLinux/python/legion_linux/legion_linux/legion_cli.py hybrid-mode-enable
-    notify-send "Ukljucena integrisana graficka, restartuj komp"
+    notify-send "Igpu - ON, Restart required."
 fi
