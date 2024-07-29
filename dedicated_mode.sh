632c7d6314855a966a2b6b686cebbd1af4a83a09
FrayedBlade <kestrel_o7@proton.me>
2024-07-29 23:28:10 +0000

    Update dedicated_mode.sh

--- dedicated_mode.sh
+++ dedicated_mode.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 if [ -f /etc/modprobe.d/vfio.conff ]; then
-    notify-send "Laptop je vec u dedicated modu!"
+    notify-send "Laptop already in dedicated mode!"
 else
     sudo mv /etc/modprobe.d/vfio.conf /etc/modprobe.d/vfio.conff
-    #postavlja grafu u dedicated mode
+    
+    #turns on dedicated mode in LLL
     sudo /home/kestrel/Programs/Toolkit/LenovoLegionLinux/python/legion_linux/legion_linux/legion_cli.py hybrid-mode-disable
-    notify-send "Ukljucena Nvidia grafa, integrusa ugasena, restartuj komp"
+    notify-send "Nvidia - ON, Restart required."
 fi
